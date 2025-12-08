2c404c152dbab5d6e8e3268c0dc81852fb779cf7
Ephraim Mgbebu <mgbebuephraim@gmail.com>
2025-12-08 23:49:29 +0100

    added new js code

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-const navBtn = document.querySelector('.jsBtn');
-const navigatioin = document.querySelector('.main-container');
-let open = document.querySelector('.nav-open');
-let close = document.querySelector('.nav-close');
+const navBtn = document.querySelector(".jsBtn");
+const navigatioin = document.querySelector(".main-container");
+let open = document.querySelector(".nav-open");
+let close = document.querySelector(".nav-close");
 
 let isopen = false;
 
@@ -9,12 +9,27 @@ navBtn.onclick = function () {
   isopen = !isopen;
 
   if (isopen) {
-    navigatioin.classList.add('active');
-    open.style.display = 'none';
-    close.style.display = 'block';
+    navigatioin.classList.add("active");
+    open.style.display = "none";
+    close.style.display = "block";
   } else {
-    navigatioin.classList.remove('active');
-    open.style.display = 'block';
-    close.style.display = 'none';
+    navigatioin.classList.remove("active");
+    open.style.display = "block";
+    close.style.display = "none";
   }
 };
+
+// hide side nav on clicks
+const navBox = document.querySelector(".sidenav");
+
+document.addEventListener("click", function (e) {
+  const clickedInsideNav = navBox.contains(e.target);
+  const clickedBtn = navBtn.contains(e.target);
+
+  if (isopen && !clickedInsideNav && !clickedBtn) {
+    navigatioin.classList.remove("active");
+    open.style.display = "block";
+    close.style.display = "none";
+    isopen = false;
+  }
+});
